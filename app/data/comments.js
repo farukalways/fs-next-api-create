18335b152712dc4a8980b8a691b6f7658e4a42b3
farukalways <alishopon367@gmail.com>
2026-02-01 11:26:15 +0600

    create api GET POST function

---
+++ app/data/comments.js
@@ -0,0 +1,16 @@
+const comments = [
+  {
+    id: 1,
+    text: "comments 1",
+  },
+  {
+    id: 2,
+    text: "comments 2",
+  },
+  {
+    id: 3,
+    text: "comments 3",
+  },
+];
+
+export default comments;
